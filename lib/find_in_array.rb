43fdb33b4e90c3fc63409a6df066b4071b83c2ee
Sisanwunmi Agbeyegbe <agbeyegbesisan@gmail.com>
2020-08-27 00:06:12 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,9 +1,11 @@
 def find_element_index(array, value_to_find)
   count = 0 
+  found_value_index = nil
   
   while count < array.length do
+    if 
     puts array[count] == value_to_find
     count += 1
   end
-  array[count].index(value_to_find)
+  
 end
